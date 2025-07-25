cb9be2a0b614e71c5f927d1cc8cc18367b0eed51
Marina Magdy <marina.magdy@twistmena.com>
2025-07-25 15:46:57 +0300

    - Commit

--- lib/features/admin/attendance/presentation/class_attendance_view.dart
+++ lib/features/admin/attendance/presentation/class_attendance_view.dart
@@ -15,17 +15,19 @@ class ClassAttendanceView extends StatefulWidget {
 
 class _ClassAttendanceViewState extends State<ClassAttendanceView> {
   late List<bool> selectedList;
-  int classCount = 15;
+  int classCount = 25;
   int attendance = 0;
+
   @override
   void initState() {
     super.initState();
     selectedList = List.generate(classCount, (_) => false);
   }
+
   @override
   Widget build(BuildContext context) {
-    int absence = classCount - attendance;
     return MyCustomScaffold(
+      // app bar
       appBarTitle: StringsManager.age,
       withBackArrow: true,
       body: Stack(
@@ -34,42 +36,22 @@ class _ClassAttendanceViewState extends State<ClassAttendanceView> {
             padding: REdgeInsets.symmetric(horizontal: 8.0),
             child: Column(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(dateOfToday()),
-                    Text(StringsManager.todayDate),
-                  ],
-                ),
+                // date
+                date(),
+
+                // just space
                 SizedBox(height: 20.h),
-                SingleChildScrollView(
-                  child: GridView.builder(
-                    shrinkWrap: true,
-                    physics: AlwaysScrollableScrollPhysics(),
-                    itemCount: 15,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      crossAxisSpacing: 16.w,
-                      mainAxisSpacing: 16.h,
-                      childAspectRatio: 0.55,
-                    ),
-                    itemBuilder: (context, index) {
-                      return CustomAttendanceContainer(
-                        isSelected: selectedList[index],
-                        onTap: (){
-                          setState(() {
-                            selectedList[index] = !selectedList[index];
-                            attendance += selectedList[index] ? 1 : -1;
-                          });
-                        },
-                      );
-                    },
-                  ),
-                ),
-                SizedBox(height: 20.h),
+
+                // class list
+                classList(),
+
+                // just space
+                SizedBox(height: 180.h),
               ],
             ),
           ),
+
+          // attendance and save button
           Positioned(
             bottom: 0,
             left: 0,
@@ -82,23 +64,7 @@ class _ClassAttendanceViewState extends State<ClassAttendanceView> {
                 child: Column(
                   mainAxisSize: MainAxisSize.min,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "$absence : ${StringsManager.absenceCount}",
-                          style: TextStyle(
-                            fontFamily: StringsManager.fontFamily,
-                            color: Colors.red,
-                          ),
-                        ),
-                        Text(
-                          "$attendance : ${StringsManager.attendanceCount}",
-                          style: TextStyle(color: Colors.green),
-                        ),
-                        Text("$classCount : ${StringsManager.classCount}"),
-                      ],
-                    ),
+                    calculateAttendance(),
                     CustomButton(onTap: () {}, title: StringsManager.save),
                   ],
                 ),
@@ -110,9 +76,63 @@ class _ClassAttendanceViewState extends State<ClassAttendanceView> {
     );
   }
 
+  date() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [Text(dateOfToday()), Text(StringsManager.todayDate)],
+    );
+  }
+
   dateOfToday() {
     String date =
         "${DateTime.now().day}/ ${DateTime.now().month}/ ${DateTime.now().year}";
     return date;
+  }
+
+  classList() {
+    return Expanded(
+      child: GridView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: classCount,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 16.w,
+          mainAxisSpacing: 16.h,
+          childAspectRatio: 0.55,
+        ),
+        itemBuilder: (context, index) {
+          return CustomAttendanceContainer(
+            isSelected: selectedList[index],
+            onTap: () {
+              setState(() {
+                selectedList[index] = !selectedList[index];
+                attendance += selectedList[index] ? 1 : -1;
+              });
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  calculateAttendance() {
+    int absence = classCount - attendance;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          "$absence : ${StringsManager.absenceCount}",
+          style: TextStyle(
+            fontFamily: StringsManager.fontFamily,
+            color: Colors.red,
+          ),
+        ),
+        Text(
+          "$attendance : ${StringsManager.attendanceCount}",
+          style: TextStyle(color: Colors.green),
+        ),
+        Text("$classCount : ${StringsManager.classCount}"),
+      ],
+    );
   }
 }
